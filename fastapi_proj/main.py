45107b58dd54f66b0fdd15ab438e7c030cfd6e61
Muhammad Asif <programmerasif@outlook.com>
2025-05-18 23:09:34 +0500

    multi variable rout

--- fastapi_proj/main.py
+++ fastapi_proj/main.py
@@ -8,3 +8,13 @@ def greet():
 @app.get("/items/{item_id}")
 def get_item(item_id: int):
     return {"item_id": item_id} 
+
+
+@app.get("/files/{file_path:path}")
+async def read_file(file_path: str):
+    return {"file_path": file_path}
+
+
+@app.get("/{product}/items/{item_id}")
+def get_item1(item_id: int, product: str):
+    return {"item_id": item_id, "product": product} 
