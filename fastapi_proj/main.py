70d5bf53aec7ca0009528a3c297961aacfb5fbf6
Muhammad Asif <programmerasif@outlook.com>
2025-05-18 22:52:56 +0500

    path parameter added

--- fastapi_proj/main.py
+++ fastapi_proj/main.py
@@ -1,5 +1,10 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 @app.get("/")
 def greet():
  return "Hello? World?"
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int):
+    return {"item_id": item_id} 
