b26c48de1efc61d43fa4e0703550ee98e3b36ea0
Muhammad Asif <programmerasif@outlook.com>
2025-05-19 09:13:08 +0500

    db

--- fastapi_proj/demo.py
+++ fastapi_proj/demo.py
@@ -10,3 +10,13 @@ def greet():
 @app.get("/items")
 def greet(q:int = 0, skip:int = 10):
  return ("Q", q, "Skip", skip)
+
+
+
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
--- fastapi_proj/query.py
+++ fastapi_proj/query.py
@@ -1,6 +1,6 @@
 import requests 
 
-res = requests.get("http://127.0.0.1:8000/items/?q=12&skip=20")
+res = requests.get("http://127.0.0.1:8000/items/?skip=0&limit=2")
 
 
 print("Text", res.text)
