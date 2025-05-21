ebcbcc0f9c5f0ab1910243d0566fb92f1cbafb95
Muhammad Asif <programmerasif@outlook.com>
2025-05-21 09:04:13 +0500

    dependency added

---
+++ fastapi_proj/depends.py
@@ -0,0 +1,14 @@
+
+from fastapi import Depends, FastAPI
+from dependencies import common_parameters
+
+app = FastAPI()
+
+@app.get("/items/")
+async def read_items(commons: dict = Depends(common_parameters)):
+    return commons
+
+
+@app.get("/users/")
+async def read_users(commons: dict = Depends(common_parameters)):
+    return commons
